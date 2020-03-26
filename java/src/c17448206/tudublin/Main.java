3bf50577ae1bc45c4180f972a8356f9d6a969b19
David Tilson <davidrtilson@gmail.com>
2020-03-26 15:37:23 +0000

    work started

---
+++ java/src/c17448206/tudublin/Main.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+import example.CubeVisual;
+import example.MyVisual;
+
+public class Main
+{	
+
+	public void startUI()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new CubeVisual());		
+	}
+
+	public static void main(String[] args)
+	{
+		Main main = new Main();
+		main.startUI();			
+	}
+}
